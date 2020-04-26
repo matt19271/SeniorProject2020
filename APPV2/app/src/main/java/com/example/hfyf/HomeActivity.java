84eb35d02eee066e285428ff78749170ffcd62c2
frankienglish <frankienglish0416@gmail.com>
2020-04-25 17:42:43 -0700

    Added new android project

---
+++ APPV2/app/src/main/java/com/example/hfyf/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.hfyf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLogout = findViewById(R.id.button3);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+
+    }
+}
---
+++ APPV2/app/src/main/java/com/example/hfyf/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.hfyf;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText emailId, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignUp = findViewById(R.id.button);
+        tvSignIn = findViewById(R.id.textView);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if (email.isEmpty()){
+                    emailId .setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else if (pwd.isEmpty() || pwd.length() < 7){
+                    password.setError("please enter password or check length is at least 7 characters");
+                    password.requestFocus();
+                }
+                else if(email.isEmpty() && pwd.isEmpty() )
+                {
+                    Toast.makeText(MainActivity.this,"Fields are empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this,"Sign up not successful, please try again", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();}
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
